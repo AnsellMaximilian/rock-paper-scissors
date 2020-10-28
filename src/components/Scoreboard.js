7d37cc8954dc6d02f996fdea00122aabe9501d40
Ansell Maximilian <ansellmaximilian@gmail.com>
2020-10-28 10:44:34 +0700

    add styling

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -7,11 +7,13 @@ class Scoreboard extends React.Component {
         return(
             <div id="scoreboard">
                 <div id="computer-score">
+                    <div id="computer-icon"></div>
                     <span id="computer-score-text">{this.props.computerScore}</span>
                 </div>
 
                 <div id="player-score">
                     <span id="player-score-text">{this.props.playerScore}</span>
+                    <div id="player-icon"></div>
                 </div>
             </div>
         )
